fa4ec4a96b82bb09a219b0a5bfdcdcd7668e9ae7
Jim Manton <jrman@risebroadband.net>
2022-12-10 18:52:52 -0700

    Update packages.

--- test/package.js
+++ test/package.js
@@ -4,14 +4,14 @@ const assert = require('assert'),
 
 const packageMock = {
   "name": "queuejson",
-  "version": "7.0.4",
+  "version": "7.0.5",
   "description": "Process class objects according to json array input data.",
   "main": "app.ts",
   "dependencies": {
     "chai": "^4.3.7",
     "mocha": "^10.1.0",
     "node-console-colors": "^1.1.4",
-    "queueobj": "^9.1.1",
+    "queueobj": "^10.0.0",
     "ts-node": "^10.9.1",
     "diffler": "^2.0.4"
   },
