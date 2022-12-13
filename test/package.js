4460803b8c3954f911e18bfb909c21dd80434073
Jim Manton <jrman@risebroadband.net>
2022-12-12 18:13:34 -0700

    Update package.json

--- test/package.js
+++ test/package.js
@@ -4,7 +4,7 @@ const assert = require('assert'),
 
 const packageMock = {
   "name": "queuejson",
-  "version": "8.0.0",
+  "version": "8.1.0",
   "description": "Process class objects according to json array input data.",
   "main": "app.js",
   "dependencies": {
